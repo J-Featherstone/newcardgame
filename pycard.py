9abca0c8ef3afcc50ab02a4d515cb11a3a486351
Joe <joeyedward.featherstone@gmail.com>
2019-07-24 22:21:21 +0100

    learning classes

---
+++ pycard.py
@@ -0,0 +1,8 @@
+import sys, pygame, random, rng
+
+colours = { "white": (255, 255, 255), "green": (0, 255, 0), "blue": (0, 0, 255), "yellow": (255, 255, 0), "black": (0, 0, 0)}
+
+
+class card:
+		def _init_(self, rarity, cost, level):
+			
---
+++ rng.py
@@ -0,0 +1,13 @@
+first_rarity = {"white": 100}
+second_rarity = {"white" : 70, "green": 20, "blue": 10}
+third_rarity = {"white": 30, "green": 30, "blue": 30, "yellow": 10}
+fourth_rarity = {"white": 20, "green": 25, "blue": 30, "yellow": 15, "black" : 10}
+
+white = []
+green = []
+blue = []
+yellow = []
+black = []
+
+class choices:
+	def present
