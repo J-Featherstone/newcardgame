7de9e7a7d323e882f59cf4fdb33bf978355cef22
Joe <joeyedward.featherstone@gmail.com>
2019-07-29 19:40:30 +0100

    select card function

--- pycard.py
+++ pycard.py
@@ -1,6 +1,6 @@
 import sys, pygame, random, rng
 pygame.init()
-colours = { "white": (255, 255, 255), "green": (0, 255, 0), "blue": (0, 0, 255), "yellow": (255, 255, 0), "black": (0, 0, 0)}
+rarities = { "common": (255, 255, 255), "uncommon": (0, 255, 0), "rare": (0, 0, 255), "epic": (255, 255, 0), "legendary": (0, 0, 0)}
 size = width, height = 1600, 1080
 
 #I did not write this i got it from stackoverflow -> allows me to add an image background
@@ -15,28 +15,28 @@ class card:
 		#def _init_(self, rarity, cost, level):
 		small_card = [64, 89]
 		large_card = [small_card[0] * 2, small_card[1] * 2]
-		card_position = []
+		position = []
 		rarity = ""
-		size = []
-		pos_rarity_size = [[],"",[]]
+		size = small_card
+		selected = False
 		#position is top left corner of card, size is small_card or large_card
-		def set_position(self, x, y):
-			self.card_position = [x,y]
-		def set_size_small(self):
-			self.size = self.small_card
-		def set_size_large(self):
-			self.size = self.large_card
-		def set_rarity(self, rareness_str):
-			self.rarity = rareness_str
-
-		def draw_card(self, position, size, rarity):
-			pos_size =  tuple(position + size)
-			pygame.draw.rect(screen, colours.get(rarity), pos_size)
-
 		def enlarge_card(self):
-			mouse_pos = pygame.mouse.get_pos()
-			for card in board.card_info:
+			self.size = self.large_card
 
+		def shrink_card(self):
+			self.size = self.small_card
+
+		def select_card(self):
+			for index, card in enumerate(board.card_info):
+				mouse_pos = pygame.mouse.get_pos()
+				if card.position[0] < mouse_pos[0] < card.position[0] + card.size[0] and card.position[1] < mouse_pos[1] < card.position[1] + card.size[1]:
+					self.selected = True
+					self.enlarge_card()
+				else:
+					self.selected = False
+					self.shrink_card()
+
+		#def shrink_card(self):
 
 		def move_card(self):
 			global held, coordinates
@@ -45,10 +45,10 @@ class card:
 				coordinates = pygame.mouse.get_pos()
 
 		def add_card(self):
-			board.cards.append([self.card_position, self.rarity, self.size])
+			board.cards.append([self.position, self.rarity, self.size])
 
-		def remove_card(self):
-			board.cards.
+		#def remove_card(self):
+			#board.cards.
 
 
 class hand:
@@ -58,29 +58,36 @@ class hand:
 	#def play_card(self):
 
 class board:
-	#card info is list of [card_position, card_rarity, card_size]
-	cards = []
+	#card info is list of [position, card_rarity, card_size]
+	card_info = []
 
-	def draw_board(self, card_info):
+	def draw_board(self):
 		screen.fill([255, 255, 255])
 		screen.blit(BackGround.image, BackGround.rect)
-		for card in card_info:
-			for prs	in card:
-				pos_size = prs[0] + prs[2]
-				pygame.draw.rect(screen, colours.get(card_rarities[k]), pos_size)
-			pygame.display.flip()
+		for card in self.card_info:
+			pos_size = card.position + card.size
+			pygame.draw.rect(screen, rarities.get(card.rarity), pos_size)
+		pygame.display.update()
 
 
 screen = pygame.display.set_mode(size)
 BackGround = Background('wood_background.jpg', [0,0])
 basic = card()
-
+basic.position = [500, 500]
+basic.rarity = "common"
+game_board = board()
+game_board.card_info.append(basic)
 while (1):
-	screen.fill([255, 255, 255])
-	screen.blit(BackGround.image, BackGround.rect)
-	basic.draw_card([800, 200], card.small_card, "white")
-	pygame.display.flip()
+	#screen.fill([255, 255, 255])
+	#screen.blit(BackGround.image, BackGround.rect)
+	#basic.draw_card([800, 200], card.small_card, "white")
+	#pygame.display.flip()
 	#if pygame.event.
+	game_board.draw_board()
+	basic.select_card()
+	#print(basic.size)
+	#if basic.selected == True:
+		#basic.enlarge_card()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
